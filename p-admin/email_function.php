a885b5957b0161ee7ac6a484dc378570b8dcd0b3
popnaja <pop.pongsak@gmail.com>
2016-06-12 16:31:35 +0700

    email

--- p-admin/email_function.php
+++ p-admin/email_function.php
@@ -175,7 +175,7 @@ function email_ct($url){
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Reset Password Request | CalForLife.com</title>
+    <title>Reset Password Request | resolutems.com</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href='$root/css/email.css' rel='stylesheet' type='text/css'/>
     <style>
